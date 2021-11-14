637e83bcf81d5653ecf346057aa5086d1a1ff114
Bitti09 <Bitti09@users.noreply.github.com>
2021-11-14 20:47:39 +0100

    remove  unused store

--- src/stores/pc.js
+++ src/stores/pc.js
@@ -2,6 +2,5 @@ import { persist, localStorage } from '@macfja/svelte-persistent-store';
 import { writable } from 'svelte/store';
 
 export let wstate = persist(writable(), localStorage(), 'wstate');
-export let platforms = persist(writable(), localStorage(), 'platforms');
 export let selectedplatform = persist(writable(), localStorage(), 'selectedplatform');
 export let selectedlang = persist(writable(), localStorage(), 'selectedlang');
